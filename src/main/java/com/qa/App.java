c7f7734958fc15d2352d81b3bc011b301d85bfc1
Sophie Olyphant <share100@hotmail.com>
2025-11-19 11:14:04 +0000

    Edit: version 3 release

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Lucy"));       
+        System.out.println(sayHelloToSomeone("Lucy"));     
+        System.out.println("Version 3 Release");   
         System.out.println(sayGoodbye());
     }
 
